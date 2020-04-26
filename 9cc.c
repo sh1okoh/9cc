e5914b9946a4439d3da68a97e286fecc90c0d865
Koki Shiozawa <kooooo.0719task@gmail.com>
2020-04-26 03:20:34 +0000

    feat: adder and substractor

--- 9cc.c
+++ 9cc.c
@@ -3,14 +3,35 @@
 
 int main(int argc, char **argv) {
   if (argc != 2) {
-    fprintf(stderr, "invalid argument");
+    fprintf(stderr, "invalid argument\n");
     return 1;
   }
 
+  char *p = argv[1];
+  
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
-  printf("main: \n");
-  printf("mov rax, %d\n", atoi(argv[1]));
-  printf(" ret\n");
+  printf("main:\n");
+  printf(" mov rax, %ld\n ", strtol(p, &p, 10));
+
+  while (*p) {
+    if (*p == '+') {
+      p++;
+      printf("  add rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    if (*p == '-') {
+      p++;
+      printf(" sub rax, %ld\n,", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "invalid argument string: '%c'\n", *p);
+    return 1;
+  }
+
+  printf(" ret\n ");
   return 0;
 }
+
